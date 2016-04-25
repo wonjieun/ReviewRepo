93b9dd9493b64c703eb6cfeb0b401b921ba737d7
Administrator <Administrator@305-19>
2016-04-25 11:17:52 +0900

    뺄셈 기능과 테스트 코드 추가

--- src/main/java/reviewproj/CalcReview.java
+++ src/main/java/reviewproj/CalcReview.java
@@ -6,6 +6,11 @@ public class CalcReview {
 	public void add(int x, int y) {
 		result=x+y;
 	}
+	
+	public void sub(int x, int y) {
+		result=x-y;
+	}
+	
 	public int getResult() {
 		return result;
 	}
--- src/test/java/reviewproj/ReviewCalcTest.java
+++ src/test/java/reviewproj/ReviewCalcTest.java
@@ -9,8 +9,15 @@ public class ReviewCalcTest {
 	@Test
 	public void testAdd() {
 		CalcReview r = new CalcReview();
-		r.add(10,  20);
+		r.add(10, 20);
 		assertEquals(30, r.getResult());	
+	}
+
+	@Test
+	public void testSub() {
+		CalcReview r = new CalcReview();
+		r.sub(10, 20);
+		assertEquals(-10, r.getResult());	
 	}
 
 }
